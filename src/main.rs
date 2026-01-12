b1c1f23878c2e07a681be6cda299a187ac121051
NaSuRall <romain.poulain@edu.esiee-it.fr>
2026-01-12 16:20:31 +0000

    ADD function to read a Vec<> Tab of CSV file and return it but i need moreee

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,17 @@ struct Personne {
 
 fn main() {
     // Fonction READER va lire le fichier et le retourner
-    let json1 = reader("tableConvert.com_ihxp8w.csv");
-    println!("json 1 = {}", json1);
-    let json2 = reader("data.csv");
-    println!("json 2 = {}", json2);
+    // let json1 = reader("tableConvert.com_ihxp8w.csv");
+    // println!("json 1 = {}", json1);
+    // let json2 = reader("data.csv");
+    // println!("json 2 = {}", json2);
 
     // Fonction READER_DEUX va lire les fichier et retourner un grand JSON
-    let json = reader_deux("tableConvert.com_ihxp8w.csv", "data.csv");
-    println!("{:#?}", json);
+    // let json = reader_deux("tableConvert.com_ihxp8w.csv", "data.csv");
+    // println!("{:#?}", json);
+
+    let files = vec!["tableConvert.com_ihxp8w.csv", "data.csv"];
+    reader_trois(&files);
 }
 
 // Fonction reader qui va avoir en paramettre "file_name" et retourner un json
@@ -57,4 +60,15 @@ fn reader_deux(file_1: &str, file_2: &str) -> String {
     next_vec.push(reader2);
 
     serde_json::to_string(&next_vec).unwrap()
+}
+
+// Fonction reader_trois va avoir en paramettre un Tableau pour y mettre autant de fichier
+// il va iterer sur le tableau recuperer et annalyser tout les fichier
+// et en faire un gros JSON
+//
+fn reader_trois(files: &Vec<&str>) {
+    for valeur in files.iter() {
+        let value = reader(&valeur);
+        println!("{}", value);
+    }
 }
